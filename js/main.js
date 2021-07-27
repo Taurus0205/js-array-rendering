3b2fea99b13fd6f92098733f0330e410a3497f49
Taurus0205 <dolchin1990@mail.ru>
2021-07-27 11:39:20 +0500

    Add genre options

--- js/main.js
+++ js/main.js
@@ -1,47 +1,73 @@
 var elList = document.querySelector(".list");
 
-for (var i = 0; i < films.length; i++) {
-  var newLi = document.createElement("li");
-  newLi.setAttribute("class", "item");
-  var newSpan = document.createElement("span");
-  newSpan.setAttribute("class", "span");
-  newSpan.textContent = films[i].id;
-  var newHeading = document.createElement("h3");
-  newHeading.setAttribute("class", "margin-bottom");
-  newHeading.textContent = films[i].title;
-  var newImg = document.createElement("img");
-  newImg.setAttribute("class", "margin-bottom");
-  newImg.setAttribute("src", films[i].poster);
-  newImg.setAttribute("width", "150");
-  newImg.setAttribute("height", "250");
+function filmsRender(films) {
+  for (var i = 0; i < films.length; i++) {
+    var newLi = document.createElement("li");
+    newLi.setAttribute("class", "item");
 
-  var newParagraph = document.createElement("p");
-  newParagraph.setAttribute("class", "margin-bottom");
-  newParagraph.textContent = films[i].overview;
+    var newSpan = document.createElement("span");
+    newSpan.setAttribute("class", "span");
+    newSpan.textContent = films[i].id;
 
-  var newDateSpan = document.createElement("span");
-  newDateSpan.setAttribute("class", "margin-bottom");
-  // release date
-  var releaseDate = new Date(films[i].release_date);
-  var day = String(releaseDate.getDate()).padStart(2, "0");
-  var month = String(releaseDate.getMonth() + 1).padStart(2, "0");
-  var year = releaseDate.getFullYear();
-  var showDay = day + "." + month + "." + year;
-  newDateSpan.textContent = showDay;
-  //
-  var newSpanGenre = document.createElement("span");
-  newSpanGenre.setAttribute("class", "bold");
+    var newHeading = document.createElement("h3");
+    newHeading.setAttribute("class", "margin-bottom");
+    newHeading.textContent = films[i].title;
 
-  for (var j = 0; j < films[i].genres.length; j++) {
-    var genreArray = films[i].genres;
-    newSpanGenre.textContent = genreArray.join(", ");
+    var newImg = document.createElement("img");
+    newImg.setAttribute("class", "margin-bottom");
+    newImg.setAttribute("src", films[i].poster);
+    newImg.setAttribute("width", "150");
+    newImg.setAttribute("height", "250");
+
+    var newParagraph = document.createElement("p");
+    newParagraph.setAttribute("class", "margin-bottom");
+    newParagraph.textContent = films[i].overview;
+
+    var newDateSpan = document.createElement("span");
+    newDateSpan.setAttribute("class", "margin-bottom");
+    // release date
+    var releaseDate = new Date(films[i].release_date);
+    var day = String(releaseDate.getDate()).padStart(2, "0");
+    var month = String(releaseDate.getMonth() + 1).padStart(2, "0");
+    var year = releaseDate.getFullYear();
+    var showDay = day + "." + month + "." + year;
+    newDateSpan.textContent = showDay;
+    //
+    var newSpanGenre = document.createElement("span");
+    newSpanGenre.setAttribute("class", "bold");
+
+    for (var j = 0; j < films[i].genres.length; j++) {
+      var genreArray = films[i].genres;
+      newSpanGenre.textContent = genreArray.join(", ");
+    }
+
+    newLi.appendChild(newSpan);
+    newLi.appendChild(newHeading);
+    newLi.appendChild(newImg);
+    newLi.appendChild(newParagraph);
+    newLi.appendChild(newDateSpan);
+    newLi.appendChild(newSpanGenre);
+    elList.appendChild(newLi);
   }
-
-  newLi.appendChild(newSpan);
-  newLi.appendChild(newHeading);
-  newLi.appendChild(newImg);
-  newLi.appendChild(newParagraph);
-  newLi.appendChild(newDateSpan);
-  newLi.appendChild(newSpanGenre);
-  elList.appendChild(newLi);
 }
+
+function filmsList(films) {
+  var genreList = [];
+  for (var i = 0; i < films.length; i++) {
+    for (var genre of films[i].genres) {
+      if (genreList.includes(genre)) {
+      } else {
+        genreList.push(genre);
+        var newOption = document.createElement("option");
+        newOption.setAttribute("value", genre);
+
+        film_genresChoice.appendChild(newOption);
+      }
+    }
+  }
+  return genreList;
+}
+
+filmsList(films);
+
+filmsRender(films);
